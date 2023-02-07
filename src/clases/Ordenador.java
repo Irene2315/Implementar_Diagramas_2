43634eba3f5644042be87b0e1198bc07c567c255
Irene Herrero <ireneherrero@outlook.com>
2023-02-07 13:43:53 +0100

    subir ultimos cambios

--- src/clases/Ordenador.java
+++ src/clases/Ordenador.java
@@ -1,5 +1,39 @@
 package clases;
 
 public class Ordenador {
+	private CPU cpu;
+	private Pantalla pantalla;
+	private Teclado teclado;
+	private Raton raton;
+	
+	
+	public CPU getCpu() {
+		return cpu;
+	}
+	public void addCpu(CPU cpu) {
+		this.cpu = cpu;
+	}
+	public Pantalla getPantalla() {
+		return pantalla;
+	}
+	public void addPantalla(Pantalla pantalla) {
+		this.pantalla = pantalla;
+	}
+	public Teclado getTeclado() {
+		return teclado;
+	}
+	public void addTeclado(Teclado teclado) {
+		this.teclado = teclado;
+	}
+	public Raton getRaton() {
+		return raton;
+	}
+	public void addRaton(Raton raton) {
+		this.raton = raton;
+	}
+	
+	
+	
+	
 
 }
